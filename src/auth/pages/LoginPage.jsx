a9a57c77623d799bcc1ffde2da51bad744a1b597
Daniel González Briseño <daniel.gb@globant.com>
2022-07-06 18:28:22 -0500

    27 - Showing Login Error Message

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,5 +1,7 @@
+import { useEffect } from 'react';
 import { useForm, useAuthStore } from '../../hooks';
 import './LoginPage.css';
+import Swal from 'sweetalert2';
 
 const loginFormFields = {
   loginEmail: 'spiderman@marvel.com',
@@ -15,16 +17,14 @@ const registerFormFields = {
 
 export const LoginPage = () => {
 
-  const { startLogin } = useAuthStore();
+  const { startLogin, errorMessage } = useAuthStore();
   
   const {
-    loginEmail, loginPassword, formState: loginFormState,
-    onInputChange: onLoginInputChange,
+    loginEmail, loginPassword, onInputChange: onLoginInputChange,
   } = useForm( loginFormFields );
 
   const {
     registerName, registerEmail, registerPassword, registerPassword2,
-    formState: registerFormState,
     onInputChange: onRegisterInputChange
   } = useForm( registerFormFields );
 
@@ -40,6 +40,12 @@ export const LoginPage = () => {
     event.preventDefault();
     console.table({ registerName, registerEmail, registerPassword, registerPassword2 });
   };
+
+  useEffect(() => {
+    if ( errorMessage !== undefined ) {
+      Swal.fire('Error en la autenticación', errorMessage, 'error');
+    }
+  }, [errorMessage]);
 
   return (
     <div className="container login-container">
