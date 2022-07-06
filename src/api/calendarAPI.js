be819ff5ffccdede936efe014bbb99de970bfe7b
Daniel González Briseño <daniel.gb@globant.com>
2022-07-06 17:26:28 -0500

    24 - Axios Configuration

---
+++ src/api/calendarAPI.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import { getEnvVariables } from '../helpers/getEnvVariables';
+
+const { VITE_API_URL } = getEnvVariables();
+
+const calendarAPI = axios.create({
+  baseURL: VITE_API_URL
+});
+
+// Configure Interceptors
+
+export default calendarAPI;
