04e31c3979398c6e34dd58a02cc26f070c28c86c
Daniel González Briseño <daniel.gb@globant.com>
2022-07-08 10:38:31 -0500

    44 - Testing Calendar Fixtures

---
+++ tests/_fixtures/calendarStates.js
@@ -0,0 +1,34 @@
+export const events = [
+  {
+    id: '1',
+    title: 'Junta con el equipo de ventas',
+    notes: 'Se trazan los objetivos a',
+    start: new Date('2022-08-15 10:00:00'),
+    end: new Date('2022-08-15 11:00:00'),
+  },
+  {
+    id: '2',
+    title: 'Junta con el equipo de desarrollo',
+    notes: 'Se planeará las los tickets para el próximo sprint',
+    start: new Date('2022-08-16 10:00:00'),
+    end: new Date('2022-08-16 11:00:00'),
+  },
+];
+
+export const initialState = {
+  isLoadingEvents: true,
+  events: [],
+  activeEvent: null
+};
+
+export const calendarWithEventsState = {
+  isLoadingEvents: false,
+  events: [ ...events ],
+  activeEvent: null
+};
+
+export const calendarWithActiveEventsState = {
+  isLoadingEvents: false,
+  events: [ ...events ],
+  activeEvent: { ...events[0] }
+};
