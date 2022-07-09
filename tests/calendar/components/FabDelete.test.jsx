6f2fe86b61eda75a150a7d0fd96523944d6ab9cf
Daniel González Briseño <daniel.gb@globant.com>
2022-07-09 14:55:54 -0500

    48 - Tests on <FabDelete />

--- tests/calendar/components/FabDelete.test.jsx
+++ tests/calendar/components/FabDelete.test.jsx
@@ -1,22 +1,85 @@
 import { render, fireEvent } from "@testing-library/react";
-import { Provider } from "react-redux";
-import { MemoryRouter } from "react-router-dom";
 import { FabDelete } from "../../../src/calendar/components/FabDelete";
-import { store } from "../../../src/store";
+import { useCalendarStore } from "../../../src/hooks/useCalendarStore";
+import { useUiStore } from "../../../src/hooks/useUiStore";
+
+jest.mock('../../../src/hooks/useCalendarStore');
+jest.mock('../../../src/hooks/useUiStore');
 
 describe('Test on <FabDelete />', () => {
-  test('Should render with defaults', () => {
-    const { container, debug } = render(
-      <Provider store={ store }>
-        <MemoryRouter>
-          <FabDelete />
-        </MemoryRouter>
-      </Provider>
+
+  const mockStartDeletingEvent = jest.fn();
+  const mockCloseDateModal = jest.fn();
+
+  beforeEach(() => jest.clearAllMocks() );
+  beforeEach(() => jest.clearAllTimers() );
+  
+  test('Should render with default values', () => {
+
+    useCalendarStore.mockReturnValue({
+      hasEventSelected: false
+    });
+  
+    useUiStore.mockReturnValue({
+      isModalClose: false,
+    });
+    
+    const { container } = render(
+      <FabDelete />
     );
 
     const btn = container.querySelector('#delete-btn');
-    btn.style.display = '';
+    
+    expect( btn.classList ).toContain('btn');
+    expect( btn.classList ).toContain('btn-danger');
+    expect( btn.classList ).toContain('fab-danger');
+    expect( btn.style.display ).toBe('none');
 
-    debug();
   });
+
+  test('Should show if active event set', () => {
+
+    useCalendarStore.mockReturnValue({
+      hasEventSelected: true
+    });
+  
+    useUiStore.mockReturnValue({
+      isModalClose: true,
+    });
+    
+    const { container } = render(
+      <FabDelete />
+    );
+
+    const btn = container.querySelector('#delete-btn');
+
+    expect( btn.style.display ).toBe('');
+
+  });
+
+  test('Should call startDeletingEvent and closeDateModal actions', () => {
+
+    useCalendarStore.mockReturnValue({
+      startDeletingEvent: mockStartDeletingEvent,
+      hasEventSelected: true
+    });
+  
+    useUiStore.mockReturnValue({
+      closeDateModal: mockCloseDateModal,
+      isModalClose: true,
+    });
+    
+    const { container, debug } = render(
+      <FabDelete />
+    );
+
+    const btn = container.querySelector('#delete-btn');
+
+    fireEvent.click(btn);
+
+    expect( mockStartDeletingEvent ).toHaveBeenCalled();
+    expect( mockCloseDateModal ).toHaveBeenCalled();
+
+  });
+
 });
